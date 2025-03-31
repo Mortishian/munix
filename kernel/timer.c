dbd0ae5e9e15c1c3ef3e6cb92530e2afb181db39
Mortishian <mortishian@proton.me>
2025-03-31 22:09:38 +0300

    Forgot to format some stuff

--- kernel/timer.c
+++ kernel/timer.c
@@ -3,13 +3,15 @@
 
 volatile int tick = 0;
 
-void timer_irq()
+void 
+timer_irq ()
 {
     tick++;
     outb(0x20, 0x20);
 }
 
-void print_ticks(void)
+void 
+print_ticks (void)
 {
     printk("%d", tick);
 }
