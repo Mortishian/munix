f046eb1c9bd507ab8f568cd985d2f7975041edb7
tish/razu <mortishian@proton.me>
2025-04-02 22:41:02 +0300

    mm: page frame allocator

--- mm/memory.c
+++ mm/memory.c
@@ -2,8 +2,13 @@
 #include <sys/console.h>
 #include <sys/panic.h>
 #include <system.h>
+#include <stddef.h>
+#include <stdint.h>
 
 int total_memory, available_memory;
+
+/* We could track around 4gb of memory */
+unsigned char frame_bitmap[131072];
 
 /* This parses the memory map given by GRUB, and performs other tasks (as well
  * as, checks if we even got one */
@@ -29,7 +34,40 @@ mem_init (multiboot_info_t *mbd)
             available_memory = available_memory + mmmt->len;
         }
     }
+    /* We'll mark all free memory as free in the page bitmap */
+    for (int i = 0; i < available_memory / 4096; i++)
+    {
+        frame_bitmap[i] = 0;
+    }
+    
     /* Most of the time we'll be dealing with these numbers in bytes though*/
     printk ("%dkb total, %dkb free\n", total_memory / 1024,
         available_memory / 1024);
+}
+
+/* Here goes the page frame allocator. For now, the kernel is flat memory model, but we still will use this allocator for some stuff. */
+
+void *alloc_page_frame()
+{
+    for (int i = 0; i < 131072; i++)
+    {
+        if (frame_bitmap[i] == 0)
+        {
+            frame_bitmap[i] = 1;
+            return (void *)(i * 4096);
+        }
+    }
+    printk("pfa: not enough memory, returning null\n");
+    return NULL;
+}
+
+void free_page_frame (void *addr)
+{
+    uint32_t frame = ((uint32_t)addr) / 4096;
+    if (frame < 131072)
+    {
+        frame_bitmap[frame] = 0;
+        return;
+    }
+    printk("pfa: frame addr not in bitmap range\n");
 }
